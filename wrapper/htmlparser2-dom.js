5272112660ca5eff1cb7cea418cfb3a45702131d
Felix Böhm <me@feedic.com>
2013-08-10 18:25:22 +0200

    added htmlparser2-dom.js, to benchmark DOM creation

---
+++ wrapper/htmlparser2-dom.js
@@ -0,0 +1,10 @@
+var htmlparser2 = require('htmlparser2'),
+    Parser = htmlparser2.Parser,
+    Handler = htmlparser2.DomHandler;
+
+module.exports = function (html, callback) {
+  var handler = new Handler();
+	var parser = new Parser(handler);
+	parser.parseComplete(html);
+	callback(null);
+};
