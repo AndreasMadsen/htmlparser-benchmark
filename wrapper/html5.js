477b51845dafb63862c8dd2ca1203cf994de2862
fb55 <me@feedic.com>
2014-04-19 19:59:34 +0200

    updated html5 wrapper
    following @danyaPostfactum's suggestions

--- wrapper/html5.js
+++ wrapper/html5.js
@@ -1,8 +1,16 @@
 
-var Parser = require('html5').Parser;
+var Parser = require('html5').SAXParser;
 
 module.exports = function (html, callback) {
 	var parser = new Parser();
+	var noop = function() {};
+	parser.contentHandler = {
+		startDocument: noop,
+		endDocument: noop,
+		startElement: noop,
+		endElement: noop,
+		characters: noop
+	};
 	parser.parse(html);
 	callback();
 };
