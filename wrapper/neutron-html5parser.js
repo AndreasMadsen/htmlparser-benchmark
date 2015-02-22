eac44d392ed383eb6610d3c9edfdf9c3fa9bf8d2
Munawwar Firoz <munawwarfiroz@hotmail.com>
2015-02-22 23:09:37 +0400

    Added neutron-html5parser benchmark.

---
+++ wrapper/neutron-html5parser.js
@@ -0,0 +1,14 @@
+var HTMLtoDOM = require("neutron-html5parser");
+
+module.exports = function (html, callback) {
+    var noop = function () {};
+    HTMLtoDOM.Parser(html, {
+        start: noop,
+        end: noop,
+        chars: noop,
+        comment: noop,
+        doctype: noop
+    });
+
+    callback(null);
+};
