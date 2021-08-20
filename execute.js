17122a5dc381d5b4d6bc8b3d17acf39687fc4e28
Felix Böhm <188768+fb55@users.noreply.github.com>
2021-08-20 22:48:28 +0100

    Save stats to a file, from CI

--- execute.js
+++ execute.js
@@ -1,48 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
+const fork = require('child_process').fork;
 
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
-var fork = require('child_process').fork;
+const wrappers = fs
+    .readdirSync(path.join(__dirname, 'wrapper'))
+    .map((filename) => ({
+        name: path.basename(filename, '.js'),
+        parser: path.join(__dirname, 'wrapper', filename),
+    }));
 
-var wrappers = fs.readdirSync(path.join(__dirname, 'wrapper'))
-	.map(function (filename) {
-		return {
-			name: path.basename(filename, '.js'),
-			parser: path.join(__dirname, 'wrapper', filename)
-		};
-	});
+const MAX_WIDTH = Math.max(...wrappers.map((wrapper) => wrapper.name.length));
 
-var MAX_WIDTH = Math.max.apply(Math, wrappers.map(function (wrapper) {
-	return wrapper.name.length;
-}));
-
-function equalWidth(name) {
-	var left = MAX_WIDTH - name.length;
-	var str = name;
-	for (var i = 0; i < left; i++) str += ' ';
-	return str;
-}
+const stats = [];
 
 async.eachSeries(
-	wrappers,
-	function (item, done) {
-		var runner = fork(path.join(__dirname, '_run.js'));
-		runner.send(item);
-		runner.on('message', function (stat) {
-			console.log(
-				'\n%s: %s ms/file ± %s',
-				equalWidth(item.name),
-				stat.mean.toPrecision(6),
-				stat.sd.toPrecision(6)
-			);
-		});
+    wrappers,
+    function (item, done) {
+        const runner = fork(path.join(__dirname, '_run.js'));
 
-		runner.on('close', function (n) {
-			if (n) {
-				console.log('%s failed (exit code %d)', item.name, n);
-			}
-			done();
-		});
-	},
-	function () {}
+        runner.send(item);
+        runner.on('message', function (stat) {
+            stats.push({ stat, name: item.name });
+            console.log(
+                '\n%s: %s ms/file ± %s',
+                item.name.padStart(MAX_WIDTH),
+                stat.mean.toPrecision(6),
+                stat.sd.toPrecision(6)
+            );
+        });
+
+        runner.on('close', function (n) {
+            if (n) {
+                console.log('%s failed (exit code %d)', item.name, n);
+            }
+            done();
+        });
+    },
+    function () {
+        // Write all stats to a text file, ordered by mean time
+        fs.writeFileSync(
+            path.join(__dirname, 'stats.txt'),
+            stats
+                .sort((a, b) => a.stat.mean - b.stat.mean)
+                .map(
+                    ({ stat, name }) =>
+                        `${name.padEnd(MAX_WIDTH)}: ${stat.mean.toPrecision(
+                            6
+                        )} ms/file ± ${stat.sd.toPrecision(6)}`
+                )
+                .join('\n')
+        );
+    }
 );
