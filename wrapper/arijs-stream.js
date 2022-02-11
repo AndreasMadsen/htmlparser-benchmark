8205606ca1a0508b0c92223a2c765f42fc66eeb3
Rafael Hengles <rafael@hengles.net>
2022-02-11 15:48:43 -0300

    Add @arijs/stream-xml-parser to the benchmark (#29)

---
+++ wrapper/arijs-stream.js
@@ -0,0 +1,10 @@
+
+var Parser = require("@arijs/stream-xml-parser").XMLParser;
+
+module.exports = function (html, callback) {
+	var parser = new Parser(nop);
+	parser.end(html);
+	callback();
+};
+
+function nop() {}
